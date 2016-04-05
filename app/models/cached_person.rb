018738c7208dc652a23d9e7a8166ace447345d6b
Vincent Budrovich <vwb@berkeley.edu>
2016-04-05 16:59:13 -0700

    Modifiable FullContact#person

--- app/models/cached_person.rb
+++ app/models/cached_person.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: cached_people
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  data       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class CachedPerson < ActiveRecord::Base
 end
---
+++ config/initializers/fullcontact_init.rb
@@ -0,0 +1,26 @@
+require 'fullcontact'
+
+FullContact.configure do |config|
+    config.api_key = ENV["fullcontact_api_key"]
+end
+
+module FullContact
+  class Client
+    module Person
+      # Returns extended information for a given person (email, phone, twitter or facebook)
+
+      def person(options={}, faraday_options={})
+        if options.is_a?(String)
+          raise ArgumentError, "Supplying an email address directly is not supported. Please use {email: #{options}} instead."
+        end
+
+        if options.has_key?(:facebookUsername) || options.has_key?(:facebookId)
+          raise ArgumentError, "Querying by Facebook ID or username is no longer supported. Please contact support@fullcontact.com for more information."
+        end
+
+        response = get('person', options, false, faraday_options)
+        final_response = (format.to_s.downcase == 'xml') ? response['person'] : response
+      end
+    end
+  end
+end
